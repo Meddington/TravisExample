1607a8edaaaf57d7897151f6cfdeccf630a96d52
Michael Eddington <mike@dejavusecurity.com>
2016-12-25 16:06:37 -0800

    correct typo

--- cloud-generic/peach_cloud_ci_runner.py
+++ cloud-generic/peach_cloud_ci_runner.py
@@ -276,8 +276,8 @@ def goodbye():
 # -S  Specifies the location of a control socket for connection sharing
 # -i  Selects a file from which the identity (private key) for public key authentication is read
 
-os.system("ssh -f -i %s -N -T -M -L 5000:127.0.0.1:5000 -S %s %s:%d" % (peach_ssh_id, ssh_ctl_api, peach_ssh_host, peach_ssh_port))
-os.system("ssh -f -i %s -N -T -M -R 7777:127.0.0.1:7777 -S %s %s:%d" % (peach_ssh_id, ssh_ctl_target, peach_ssh_host, peach_ssh_port))
+os.system("ssh -f -i %s -p %d -N -T -M -L 5000:127.0.0.1:5000 -S %s %s" % (peach_ssh_id, peach_ssh_port, ssh_ctl_api, peach_ssh_host))
+os.system("ssh -f -i %s -p %d -N -T -M -R 7777:127.0.0.1:7777 -S %s %s" % (peach_ssh_id, peach_ssh_port, ssh_ctl_target, peach_ssh_host))
 
 # Start job
 try:
@@ -297,7 +297,7 @@ except Exception as ex:
 
 # Start SSH to Peach Proxy
 
-os.system("ssh -f -i %s -N -T -M -L %d:127.0.0.1:%d -S %s %s:%d" % (peach_ssh_id, proxy_port, proxy_port, ssh_ctl_proxy, peach_ssh_host, peach_ssh_port))
+os.system("ssh -f -i %s -p %d -N -T -M -L %d:127.0.0.1:%d -S %s %s" % (peach_ssh_id, peach_ssh_port, proxy_port, proxy_port, ssh_ctl_proxy, peach_ssh_host))
 
 try:
     
