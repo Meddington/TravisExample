3f77dd499ddabb35cf587ad242cb91dd7d699389
Michael Eddington <mike@dejavusecurity.com>
2016-12-25 17:46:28 -0800

    logging

--- cloud-generic/peach_cloud_ci_runner.py
+++ cloud-generic/peach_cloud_ci_runner.py
@@ -98,7 +98,7 @@ except:
     sys.exit(exit_code_error)
 
 # Peach API url
-peach_api = "http://127.0.0.1:5000"
+peach_api = "http://127.0.0.1:5001"
 
 # Peach UI URL
 try:
@@ -284,7 +284,7 @@ def goodbye():
 # -i  Selects a file from which the identity (private key) for public key authentication is read
 
 logger.info("Port 5000")
-os.system("ssh -f -i %s -p %d -N -T -M -L 5000:127.0.0.1:5000 -S %s %s@%s" % (peach_ssh_id, peach_ssh_port, ssh_ctl_api, peach_ssh_user, peach_ssh_host))
+os.system("ssh -f -i %s -p %d -N -T -M -L 5001:127.0.0.1:5000 -S %s %s@%s" % (peach_ssh_id, peach_ssh_port, ssh_ctl_api, peach_ssh_user, peach_ssh_host))
 logger.info("Port 7777/8888")
 os.system("ssh -f -i %s -p %d -N -T -M -R 8888:127.0.0.1:7777 -S %s %s@%s" % (peach_ssh_id, peach_ssh_port, ssh_ctl_target, peach_ssh_user, peach_ssh_host))
 
